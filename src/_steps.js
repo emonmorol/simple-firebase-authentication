1764bd43b9ed47e81f9b8ffcb7d7fb4892267b47
emonmorol <ekramulhasane69@gmail.com>
2022-04-08 21:37:08 +0600

    all done

--- src/_steps.js
+++ src/_steps.js
@@ -3,8 +3,14 @@
 *steps to use firebase
 
 1. Create a project on console.firebase.google.com
-2. Install firebase using npm install firebase
-3.register web app in firebase
-4.copy firebase init with config from firebase project settings
+2. npm install firebase
+3. register web app in firebase
+4. copy firebase init with config from firebase project settings into firebase.init.js
+5. export default app
+6. import app firebase init.js into our app.js file
+7. import getAuth from firebase/auth and create auth = getAuth(app)
+8. Turn on google authentication(firebase>authentication>enable google sign in)
+9. create google provider
+10. sign in with pop up
 
 */
